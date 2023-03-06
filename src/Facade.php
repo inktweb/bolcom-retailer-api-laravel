d6e2727a643ab1b3586e2d4bcaef72c64eacac84
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2023-03-06 15:43:51 +0100

    Added missing @throws; added type declaration; added all available API versions to return type docblock

--- src/Facade.php
+++ src/Facade.php
@@ -2,27 +2,39 @@
 
 namespace Inktweb\Bolcom\RetailerApi\Laravel;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Facade as LaravelFacade;
 use Inktweb\Bolcom\RetailerApi\Client\Config;
 use Inktweb\Bolcom\RetailerApi\Contracts\Client;
 use Inktweb\Bolcom\RetailerApi\Laravel\Exceptions\ConfigException;
+use Inktweb\Bolcom\RetailerApi\Clients;
 
 class Facade extends LaravelFacade
 {
     /** @var Client[] */
-    protected static $accounts = [];
+    protected static array $accounts = [];
 
     protected static function getFacadeAccessor(): string
     {
         return 'bolcom-retailer-api';
     }
 
+    /**
+     * @return Client|Clients\V7\Client|Clients\V8\Client
+     * @throws ConfigException
+     * @throws BindingResolutionException
+     */
     public static function account(string $name): Client
     {
         return static::$accounts[$name]
             ?? static::$accounts[$name] = static::getAccount($name);
     }
 
+    /**
+     * @return Client|Clients\V7\Client|Clients\V8\Client
+     * @throws BindingResolutionException
+     * @throws ConfigException
+     */
     protected static function getAccount(string $name): Client
     {
         $config = static::getFacadeApplication()->make('config')->get('bolcom-retailer-api');
